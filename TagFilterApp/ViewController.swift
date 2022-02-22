edf31f68b8c57641645da1f27fa8b4ca0f310911
Juan C <juanceballos@pursuit.org>
2022-02-21 19:01:58 -0500

    first commit

---
+++ TagFilterApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TagFilterApp
+//
+//  Created by Juan Ceballos on 2/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
